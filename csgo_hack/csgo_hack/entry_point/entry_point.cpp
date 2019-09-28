f3274a67b7bcd06c48ba95e43fa845ef2fbfe5ad
pSilent <fcknggers1488@gmail.com>
2019-09-27 19:41:27 -0500

    initial_commit

---
+++ csgo_hack/csgo_hack/entry_point/entry_point.cpp
@@ -0,0 +1,15 @@
+#include "../source_development_kit/source_development_kit.h"
+
+void start_hack()
+{
+	MessageBox(NULL, "cum_daddy", "niggers_lynched.dll", MB_OK);
+}
+
+// copyright_microsoft_2019 (developer.microsoft.com/en-us/windows) (((not my code)))
+BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH: start_hack(); break;
+	}
+}
