1b27e5c965db212a8efed0400b43619ea972c555
pSilent <fcknggers1488@gmail.com>
2019-09-28 10:39:47 -0500

    fixed incorrectly named files

--- csgo_hack/csgo_hack/source_development_kit/source_development_kit.hh
+++ csgo_hack/csgo_hack/source_development_kit/source_development_kit.hh
@@ -6,7 +6,7 @@
 #include "../console/console.hh"
 #include "../source_development_kit/utilities/utilities.hh"
 
-#include "../source_development_kit/valve/recv.h"
-#include "../source_development_kit/valve/c_client.h"
+#include "../source_development_kit/valve/recv.hh"
+#include "../source_development_kit/valve/c_client.hh"
 
 #include "../source_development_kit/interfaces/interfaces.hh"
